2b6914f74105dcb3096948f25c8eae8438941490
Benjamin <benghent@gmail.com>
2020-10-19 14:13:45 -0500

    Auto
    Start to convert to the new auto class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBlue_Basic_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBlue_Basic_Auto.java
@@ -8,22 +8,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcontroller.external.samples.SensorREV2mDistance;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 @Autonomous(name="Red_Blue_Basic_Auto", group="Linear Opmode")
 //@Disabled
 public class RedBlue_Basic_Auto extends LinearOpMode {
 
     Hardware r = new Hardware();
+
+    Auto a = new Auto();
 
     @Override
     public void runOpMode() {
@@ -33,16 +24,6 @@ public class RedBlue_Basic_Auto extends LinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-
-        //3.1 per deg
-
-        r.frontLeft.setTargetPosition(558);
-
-        r.frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        do r.frontLeft.setPower(0.6); while(
-            r.frontLeft.getCurrentPosition() < r.frontLeft.getTargetPosition()
-        );
 
     }
 }
