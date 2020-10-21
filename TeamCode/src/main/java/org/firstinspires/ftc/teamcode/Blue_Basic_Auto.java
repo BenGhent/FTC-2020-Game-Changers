2dc3687c22433886c069456f7ea40534f5cbda64
Benjamin <benghent@gmail.com>
2020-10-21 10:41:28 -0500

    Blue side basic
    Auto for the blue side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue_Basic_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue_Basic_Auto.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Blue_Basic_Auto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous(name="Blue_Basic_Auto", group="Linear Opmode")
+//@Disable
+public class Blue_Basic_Auto extends Linear_OpMode {
+
+    Hardware r = new Hardware();
+
+    Auto a = new Auto();
+
+    @Override
+    public void runOpMode() {
+        AutoTransitioner.transitionOnStop(this, "TeleOp_Basic");
+        r.initRobot(hardwareMap, telemetry);
+        r.initAutonomous();
+
+        r.side = 0;
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+    }
 }
