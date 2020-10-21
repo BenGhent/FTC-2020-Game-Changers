93e89e4a64a4bf7657387c39b1f6934f9d19fa98
Benjamin <benghent@gmail.com>
2020-10-21 10:52:20 -0500

    Red Basic

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Basic_Auto.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous(name="Red_Basic_Auto", group="Linear Opmode")
+//@Disable
+public class Red_Basic_Auto extends LinearOpMode {
+
+    Hardware r = new Hardware();
+
+    Auto a = new Auto();
+
+    @Override
+    public void runOpMode() {
+        AutoTransitioner.transitionOnStop(this, "TeleOp_Basic");
+        r.initRobot(hardwareMap, telemetry);
+        r.initAutonomous();
+
+        r.side = 0; //Blue side
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+    }
+}
