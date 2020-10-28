10121627f004ef90695c059dfdb2b7139c9c4b9c
Benjamin <benghent@gmail.com>
2020-10-28 13:36:45 -0500

    Test
    This was to test how to use the control hub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/Blue_Basic_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/Blue_Basic_Auto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Autos;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.AutoTransitioner;
 import org.firstinspires.ftc.teamcode.Hardware;
@@ -24,8 +25,23 @@ public class Blue_Basic_Auto extends LinearOpMode {
 
         a.init();
 
+        r.frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+
+        double angle;
+
+        r.frontLeft.setTargetPosition((int) (r.angle(100, 200, 30)* r.ticksPerDeg));
+
+        r.frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        do{
+            r.frontLeft.setPower(0.5);
+        }while(
+                r.frontLeft.getCurrentPosition() != r.frontLeft.getTargetPosition()
+        );
+
 
     }
 }
